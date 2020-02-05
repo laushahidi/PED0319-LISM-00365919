053e3e23deb43f45452ba52976c3cb4ae2411ca3
laushahidi <00365919@uca.edu.sv>
2020-02-05 12:16:43 -0600

    Add: hw 08

--- Laboratorio 08/HW-01.cpp
+++ Laboratorio 08/HW-01.cpp
@@ -25,7 +25,9 @@ int main(void){
     do{
         cout << "Menu principal" << endl;
         cout << "1. Ingresar datos" << endl;
-        cout << "2. Salir" << endl;
+        cout << "2. Ver recorridos" << endl;
+        cout << "3. Salir" << endl;
+        cout << "Su opcion: ";
         cin >> continuar; cin.ignore();
         switch(continuar){
             case 1:
@@ -35,25 +37,28 @@ int main(void){
                 cout << "DUI: "; cin >> personData.dui; cin.ignore();
                 insertInTree(&pTree, personData);
             break;
+
             case 2:
-                cout << "Salida exitosa" << endl;
+                cout << "Imprimiendo en preOrder" << endl;
+                preOrder(pTree); cout << endl << endl;
+
+                cout << "Imprimiendo en inOrder" << endl;
+                inOrder(pTree); cout << endl << endl;
+
+                cout << "Imprimiendo en postOrder" << endl;
+                postOrder(pTree); cout << endl << endl;
             break;
+
+            case 3:
+                cout << endl << "Salida exitosa" << endl;
+            break;
+
             default:
-                cout << "Opcion invalida!" << endl;
+                cout << endl << "Opcion invalida!" << endl;
             break;
         }       
 
-    }while(continuar != 2);
-
-    cout << "Imprimiendo en preOrder" << endl;
-    preOrder(pTree); cout << endl << endl;
-
-    cout << "Imprimiendo en inOrder" << endl;
-    inOrder(pTree); cout << endl << endl;
-
-    cout << "Imprimiendo en postOrder" << endl;
-    postOrder(pTree); cout << endl << endl;
-
+    }while(continuar != 3);
 
     return 0;
 }
